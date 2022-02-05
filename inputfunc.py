8a0465066cce0cc307de1dd09b9347142ea543fc
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-05 19:47:41 +0530

    Add files via upload

---
+++ inputfunc.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+input()
+hey! Ujjawal Chourasiya Here 
+'hey! Ujjawal Chourasiya Here '
