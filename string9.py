94bc574af5ae7a78d4327d04161d297eee08b625
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-05 19:40:04 +0530

    Add files via upload

---
+++ string9.py
@@ -0,0 +1,15 @@
+#Write a script that prints out the result of .startswith("be")
+#on each of the following strings:
+#string1 = "Becomes"
+#string2 = "becomes"
+#string3 = "BEAR"
+#string4 = " bEautiful"
+string1 = "Becomes"
+string1.startswith("be")
+string2 = "becomes"
+string2.startswith("be")
+string3 = "BEAR"
+string3.startswith("be")
+string4 = " bEautiful"
+string4.startswith("be")
+
