70f67a5457e1f5281663320eae01721b58d94fdd
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-06 17:22:20 +0530

    Add files via upload

---
+++ string10.py
@@ -0,0 +1,8 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# Write a script that takes input from the user and displays the input
+#in lowercase.
+string = "UJJAWAL CHoUrasiaYA"
+string_stored = string.lower()
+print("my name is :" , string_stored)
+my name is : ujjawal chourasiaya
