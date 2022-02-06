ede1adc4eff507fee8c8bb5d5d3bc9d51b8be2cf
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-06 17:37:49 +0530

    Add files via upload

---
+++ string11.py
@@ -0,0 +1,9 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Write a script that takes input from the user and displays the number of characters inputted
+stored = input()
+hey! ujjawal chourasiya
+len(stored)
+23
+print(stored)
+hey! ujjawal chourasiya
