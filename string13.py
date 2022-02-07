ef25a45b76188d0f6ce92468082cadf31e03c658
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-07 19:34:32 +0530

    Add files via upload

---
+++ string13.py
@@ -0,0 +1,7 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Create a string object and an integer object, then display them sideby-side with a single print statement by using the str() function
+int_var = 10
+str_var = "student"
+print( str(int_var) + str_var )
+10student
