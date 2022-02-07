b8a88d91ab298c3c7c74a38e0592891d64ca1dfd
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-07 19:22:26 +0530

    Add files via upload

---
+++ string12.py
@@ -0,0 +1,12 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Create a string containing an integer, then convert that string into
+#an actual integer object using int(). Test that your new object is
+#a number by multiplying it by another number and displaying the
+#result
+string = "10"
+int(string)
+10
+string_mul = int(string)
+2*string_mul
+20
