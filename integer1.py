9d75ecb3687d199bb7bd1753c17463ccbdc54875
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-08 18:11:18 +0530

    Add files via upload

---
+++ integer1.py
@@ -0,0 +1,15 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Write a script that creates the two variables, num1 and num2. Both
+#num1 and num2 should be assigned the integer literal 25,000,000,
+#one written with underscored and one without. Print num1 and num2
+#on two separate lines
+num1 = 25,000,000
+num1
+(25, 0, 0)
+num2 = 25_000_000
+num2
+25000000
+num = 25,000,000,
+num
+(25, 0, 0)
