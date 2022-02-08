e8aa214fa99738952cb1caf07c2e6ff93987b625
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-08 18:15:00 +0530

    Add files via upload

---
+++ float1.py
@@ -0,0 +1,8 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Write a script that assigns the floating-point literal 175000.0 to the
+#variable num using exponential notation, and then prints num in the
+#interactive window
+float_num = 175e3
+float_num
+175000.0
