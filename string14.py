aff205d9601e7900cb41b67b818f6af89c1f486d
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-08 15:08:56 +0530

    Add files via upload

---
+++ string14.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#In one line of code, display the result of trying to .find() the substring "a" in the string "AAA". The result should be -1
+"hey! AAA ".find("a")
+-1
