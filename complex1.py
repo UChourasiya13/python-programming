b3cde3b141291fe8782cbb3beaf45cbe5365933c
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-09 20:05:16 +0530

    Add files via upload

---
+++ complex1.py
@@ -0,0 +1,8 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#write a one line code for complex number and print its value
+complex_num = 2 + 2j
+print(complex_num)
+(2+2j)
+type(complex_num)
+<class 'complex'>
