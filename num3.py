ea1acc5c655958b0683c912ab6fcee8ae7902b03
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-09 20:01:08 +0530

    Add files via upload

---
+++ num3.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Print the result of 2 / 10 as a percentage with no decimal places.The output should look like 20%.
+print(f"{2 / 10:.0%}")
+20%
