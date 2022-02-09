f906792259351ced3281670e9bd636c4ce99954f
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-09 19:58:20 +0530

    Add files via upload

---
+++ num1.py
@@ -0,0 +1,9 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Print the result of the calculation 3 ** .125 as a fixed-point number
+#with three decimal places
+ print(f"{3 ** .125:.3f}")
+ 
+SyntaxError: unexpected indent
+print(f"{3 ** .125:.3f}")
+1.147
---
+++ num2.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Print the number 150000 as currency, with the thousands grouped with commas. Currency should be displayed with two decimal places
+print(f"${150000:,.2f}")
+$150,000.00
