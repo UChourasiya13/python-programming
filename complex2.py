fe386e1be0aaa032274bf77f99746cf28ae49488
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-10 23:31:47 +0530

    Add files via upload

---
+++ complex2.py
@@ -0,0 +1,8 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#write a program to print the real and imaginary part of a complex number
+com_num = 29 + 2j
+com_num.real
+29.0
+com_num.imag
+2.0
