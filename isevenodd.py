74e10aa5b1d6592bd0ee14c66184a727c5c9d045
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-11 21:12:49 +0530

    Add files via upload

---
+++ isevenodd.py
@@ -0,0 +1,10 @@
+#write a code for even and odd number such that number is inputed from a user
+x = int(input("please enter a number"))
+print(type(x))
+if x%2==0:
+    print("x is a even number")
+else:
+    print("x is a odd number")
+    
+
+
