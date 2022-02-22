b6733c8cd4b9cf63496fc9e10bf63488da406a15
Ujjawal Chourasiya <89144606+coder-ujju@users.noreply.github.com>
2022-02-22 21:24:29 +0530

    Add files via upload

---
+++ abs.py
@@ -0,0 +1,6 @@
+#Write a script that asks the user to input a number and then displays the absolute value of that number. When run, your program
+#should look like this:
+#Enter a number: -10
+#The absolute value of -10 is 10.0
+num = int(input("Enter a number: "))
+print(abs(num))
---
+++ round1.py
@@ -0,0 +1,7 @@
+#Write a script that asks the user to input a number and then displays that number rounded to two decimal places. When run, your
+#program should look like this:
+#Enter a number: 5.432
+#5.432 rounded to 2 decimal places is 5.43
+inp_num = float(input("Enter a number: "))
+print(round(inp_num,2))
+
