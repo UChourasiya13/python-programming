64425de9569084ec1a7d900c5c1abc1f956cc3de
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-02-24 21:11:37 +0530

    Add files via upload

---
+++ funMul.py
@@ -0,0 +1,8 @@
+#multiply 2 number using function(with return value)
+def multiply(x , y):
+    num = x*y
+    return num
+
+x = multiply(2,3)
+print(x)
+
---
+++ funNoReturn.py
@@ -0,0 +1,7 @@
+#write a code for a function which is not returning any value
+
+def kacha(name):
+    print(int(2)*(f" kacha {name}"))
+
+
+kacha("badam")
