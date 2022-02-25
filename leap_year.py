7ccc744cb16058fc5bb1d6c5a1b77be744e147ce
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-02-25 22:10:12 +0530

    Add files via upload

---
+++ leap_year.py
@@ -0,0 +1,16 @@
+'''x =1
+while(x<11):
+    print(x)
+    x= x+1'''
+#leap year program
+year =int(input("Enter a year: "))
+if (year%400==0):
+    print("Year is a leap year")
+elif(year%100==0):
+    print("Year is not a leap year")
+if (year%4==0):
+    print("Year is a leap year")
+else:
+    print("Year is not a leap year")
+    
+    
