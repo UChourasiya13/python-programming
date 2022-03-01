7d8c6fc911f98c4014a70fc4a3e5cf06af97d198
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-03-01 12:56:02 +0530

    Add files via upload

---
+++ find_Grade_using_if_else.py
@@ -0,0 +1,13 @@
+#WAP using if else caculating grades of a student
+
+grade = int(input("Enter grade: ")) 
+if grade >= 90: 
+    print("You passed the class with grade A.")
+elif grade >= 80: 
+    print("You passed the class with grade B.")
+elif grade >= 70: 
+    print("You passed the class with grade C.")
+else: 
+    print("You did not pass the class :(")
+
+print("Thanks for attending.")
