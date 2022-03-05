eeea6e29c1e2dde9f8a5f391a387f545a02fa6ee
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-03-05 17:00:40 +0530

    Add files via upload

---
+++ local_vs_global_input2_val_in_1_line.py
@@ -0,0 +1,12 @@
+#local variable vs global variable
+
+def user_input():
+    a = int(input("Enter value 1: "))
+    b = int(input("Enter value 2: "))
+    return (a,b)
+def add(a,b):
+    return(a+b)
+a,b=user_input()
+add(a,b)
+print(a,"+",b,'=',add(a,b))    
+
