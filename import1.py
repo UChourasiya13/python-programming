0f976e97f353ccf6197487882c60738129bc0e11
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-03-07 23:52:03 +0530

    Add files via upload

---
+++ import1.py
@@ -0,0 +1,28 @@
+#IMPORTING A FUNCTION
+#1.import
+#2.import as
+#3, from___import
+
+
+#1st method
+
+'''import math
+x=4
+y=math.sqrt(x)
+print(x)
+print(y)'''
+
+'''#2nd method
+
+import math as m
+x=4
+y=m.sqrt(x)
+print(x)
+print(y)'''
+
+#3rd method
+from math import sqrt
+x=4
+y=sqrt(x)
+print(x)
+print(y)
