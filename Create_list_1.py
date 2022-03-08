5c7db6126e1b711288b9179668eb9cce4f066bfb
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-03-08 22:20:16 +0530

    Add files via upload

---
+++ Create_list_1.py
@@ -0,0 +1,11 @@
+#BASIC PROGRAM OF CREATING LIST
+a=[1,2,3,4,5]
+#OR
+b=list(a)#typecasting
+print(a)
+print(b)
+a.append(6)#adding value at last 
+print(a)
+a.insert(0,7)#adding value at respective index here first argument is index and second argument is actual value!
+print(a)
+print(a,[4])#reading value
