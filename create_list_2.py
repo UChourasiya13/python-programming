a232635cbf70e480ef5113109050861af8f95223
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-03-08 22:20:36 +0530

    Add files via upload

---
+++ create_list_2.py
@@ -0,0 +1,9 @@
+#Retriving value through list!
+
+a =['Ujjawal' ,'lig','ujjawalchourasiya40287@gmail.com','8871038950']
+print('Name is :',a[0])
+print('Place is :',a[1])
+print('gmai is :',a[2])
+print('Phone no is :',a[3])
+
+
